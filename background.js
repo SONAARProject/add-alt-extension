89d53b6079261cd1beb1a23b38cea67ddf340101
João Vicente <joao.vicente@campus.ul.pt>
2021-01-20 17:04:34 +0000

    First commit

---
+++ background.js
@@ -0,0 +1,71 @@
+const searchEndpoint = "https://accessible-serv.lasige.di.fc.ul.pt/sonaar/clarifai/search";
+const insertEndpoint = "https://accessible-serv.lasige.di.fc.ul.pt/sonaar/clarifai/insertBuffer";
+
+function searchImage() {
+  chrome.storage.local.get("imageBuffer", function(data) {
+    if(typeof data.imageBuffer === "undefined") {
+      console.log('No data')
+    } else { 
+      const http = new XMLHttpRequest();
+      http.open('POST', searchEndpoint, true);
+      http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      http.onreadystatechange = function() {
+        if(http.readyState === 4 && http.status === 200) {
+          const response = JSON.parse(http.responseText);
+          
+          if (response.status !== 4) {
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+              chrome.tabs.sendMessage(tabs[0].id, {type: "checkAltText", alts: response?.alts });
+            });
+          }
+        }
+      }
+
+      http.send("imageBuffer=" + data.imageBuffer);
+    }
+  });
+}
+
+function saveText(text) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {type: "submitButton", text });
+  });
+}
+
+function submitImage(text) {
+  chrome.storage.local.get("imageBuffer", function(data) {
+    if(typeof data.imageBuffer === "undefined") {
+      console.log('No data')
+    } else { 
+      const http = new XMLHttpRequest();
+      http.open('POST', insertEndpoint, true);
+      http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      http.onreadystatechange = function() {
+        if(http.readyState === 4 && http.status === 200) {
+          alert(http.responseText);
+        }
+      }
+
+      http.send("imageBuffer=" + data.imageBuffer + "&altText=" + encodeURIComponent(text));
+    }
+  });
+}
+
+chrome.runtime.onMessage.addListener(
+  function(message, sender, sendResponse){      
+    switch(message.type) {
+      case "search":
+        searchImage();
+        break;
+      case "save":
+        saveText(message.text);
+        break;
+      case "submit":
+        submitImage(message.text);
+        break;
+      default:
+        console.log("default message");
+        break;
+    }
+  }
+);
---
+++ content.js
@@ -0,0 +1,58 @@
+function grabPostingInput() {
+  const inputs = document.querySelectorAll("input[type=file]");
+  
+  for (const input of inputs || []) {
+    input.addEventListener("change", function() {
+      const reader = new FileReader();
+      reader.onload = function() {
+        setTimeout(function() {
+          const addDescription = document.querySelector('a[href="/compose/tweet/media"]');
+          addDescription.style["pointer-events"] = "none";
+        }, 500)
+
+        const bytes = new Uint8Array(this.result);
+        chrome.storage.local.set({ imageBuffer: JSON.stringify(bytes) });
+        chrome.runtime.sendMessage({type: "search"});
+      }
+      reader.readAsArrayBuffer(this.files[0]);
+    });
+  }
+}
+
+chrome.runtime.onMessage.addListener(
+  function(message, sender, sendResponse) {
+    if (message.type === "checkAltText") {
+      const alts = message.alts ? JSON.parse(message.alts) : undefined;
+      const addDescription = document.querySelector('a[href="/compose/tweet/media"]');
+      addDescription.style["pointer-events"] = "auto";
+      addDescription.addEventListener("click", function() {
+        setTimeout(function() {
+          const textarea = document.querySelector('textarea[name="altTextInput"]');
+          textarea.textContent = !alts ? '' : alts.map(alt => {
+            return alt.AltText.trim();
+          }).join("\n");
+
+          const save = document.querySelectorAll('div[role=button][data-focusable=true]')[1];
+          save.addEventListener("click", function() {
+            const text = !alts ? textarea.textContent.trim() :
+              textarea.textContent.trim() !== alts[0].AltText.trim() ? textarea.textContent.trim() : undefined ;
+            chrome.runtime.sendMessage({type: "save", text });
+          });
+        }, 1000);
+      });
+    } else if (message.type === "submitButton") {
+      setTimeout(function() {
+        const submitButton = document.querySelector("div[role=button][data-testid=tweetButtonInline]");
+        submitButton.addEventListener("click", function() {
+          const text = message.text;
+          
+          if (text) {
+            chrome.runtime.sendMessage({type: "submit", text });
+          }
+        });
+      }, 1000);
+    }
+  }
+);
+
+setTimeout(grabPostingInput, 2000);
