bb2b4ba54ed656eb911ab7ed81618ca16946ba96
João Vicente <joao.vicente@campus.ul.pt>
2021-02-01 12:46:36 +0000

    updated endpoints

--- endpoints.js
+++ endpoints.js
@@ -2,5 +2,5 @@
  * Change the endpoints to your local installation
  * These endpoints must use HTTPS
  */
-const searchEndpoint = "https://<change-me>/sonaar/clarifai/search";
-const insertEndpoint = "https://<change-me>/sonaar/clarifai/insertBuffer";
+const searchEndpoint = "https://<change-me>/clarifai/search";
+const insertEndpoint = "https://<change-me>/clarifai/insertBuffer";
