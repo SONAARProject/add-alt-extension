a1d233aa551bface02225adec24fe2bac49962e1
João Vicente <joao.vicente@campus.ul.pt>
2021-03-10 15:57:48 +0000

    Code updated

--- endpoints.js
+++
@@ -1,6 +0,0 @@
-/**
- * Change the endpoints to your local installation
- * These endpoints must use HTTPS
- */
-const searchEndpoint = "https://accessible-serv.lasige.di.fc.ul.pt/sonaar/clarifai/search";
-const insertEndpoint = "https://accessible-serv.lasige.di.fc.ul.pt/sonaar/clarifai/insertBuffer";
